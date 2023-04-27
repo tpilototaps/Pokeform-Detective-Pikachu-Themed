caaac094b98e363320f2a55299b0447cd932bf37
tpilototaps <tpiloto@liberty.edu>
2023-04-27 19:36:43 -0400

    Altered Config

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config():
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://tdvlmdgw:84HzWCVgv3CMJkjdBt3jp8PEXiZTMFx1@lallah.db.elephantsql.com/tdvlmdgw'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
